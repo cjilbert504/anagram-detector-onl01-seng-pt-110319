fda956e7551933013b4d501485f841defb808a05
Collin Jilbert <cjilbert504@gmail.com>
2019-12-09 01:06:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,1 @@
-class Anagram
-attr_accessor :word
 
-def initialize(word) 
-  @word = word 
-end
-
-def match(words_array) 
-  matches = []
-  words_array.select { |word| @word.split("").sort == word.split("").sort} 
-end
-end
