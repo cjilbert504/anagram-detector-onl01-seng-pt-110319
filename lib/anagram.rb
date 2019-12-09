554c39595831eb258e569ee8da1e20b7e8cb53db
Collin Jilbert <cjilbert504@gmail.com>
2019-12-09 00:59:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,3 @@
 class Anagram 
-  attr_accessor :word
 
-  def initialize(word) 
-    @word = word 
-  end
-
-  def match(words_array) 
-    matches = []
-    words_array.select { |word| @word.split("").sort == word.split("").sort} 
-  end
 end
