7725df83fad7b107487327021ccab5a156a49fe5
Collin Jilbert <cjilbert504@gmail.com>
2019-12-09 01:03:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ end
 
 def match(words_array) 
   matches = []
-  words_array.select { |word| @word.split("").sort == word.split("").sort} 
+  words_array.select { |word| @word.split(" ").sort == word.split(" ").sort} 
 end
 
 end
